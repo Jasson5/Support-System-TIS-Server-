7127e2ee55db0f14c02cadb192ec0aca10b16212
daria <reillyfernandez17@gmail.com>
2021-12-20 23:50:40 -0400

    update average procedure

--- DataAccess/Scripts/StoredProcedures/dbo.GetAverageByAttendance.sql
+++ DataAccess/Scripts/StoredProcedures/dbo.GetAverageByAttendance.sql
@@ -5,8 +5,11 @@ GO
 CREATE PROCEDURE dbo.GetAverageByAttendance
 @ShortName NVARCHAR(MAX)
 AS
-	SELECT AVG(ALL att.AttendanceGrade) as [GradeAverage], u.GivenName
+	SELECT AVG(ALL att.AttendanceGrade) as [GradeAverage], u.Id, 
+		sum(case when att.AttendanceStatus = 1 then 1 else 0 end)Presentes, 
+		sum(case when att.AttendanceStatus = 2 then 1 else 0 end)Tardes,
+		sum(case when att.AttendanceStatus = 3 then 1 else 0 end)Inasistencias
 	FROM  Attendances att,  UsersCompanies uc, Users u
 	WHERE uc.UserId = u.Id and uc.ShortName=@ShortName and att.UserId=u.Id
-	GROUP BY (u.GivenName)
+	GROUP BY (u.Id)
 GO
