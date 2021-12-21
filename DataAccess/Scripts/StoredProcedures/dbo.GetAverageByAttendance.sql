aa9a76b84631f8554524293373faddad7dff8345
daria <reillyfernandez17@gmail.com>
2021-12-20 23:25:41 -0400

    new procedure average

---
+++ DataAccess/Scripts/StoredProcedures/dbo.GetAverageByAttendance.sql
@@ -0,0 +1,12 @@
+﻿IF OBJECT_ID('dbo.GetAverageByAttendance', 'P') IS NOT NULL
+	DROP PROCEDURE dbo.GetAverageByAttendance
+GO
+
+CREATE PROCEDURE dbo.GetAverageByAttendance
+@ShortName NVARCHAR(MAX)
+AS
+	SELECT AVG(ALL att.AttendanceGrade) as [GradeAverage], u.GivenName
+	FROM  Attendances att,  UsersCompanies uc, Users u
+	WHERE uc.UserId = u.Id and uc.ShortName=@ShortName and att.UserId=u.Id
+	GROUP BY (u.GivenName)
+GO
